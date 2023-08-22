f0673072c7996320d396faf56f61e7a7ea5cb606
Shamoche <alakindeakintomiwa@gmail.com>
2023-08-22 05:14:56 -0700

    printf

--- _printf.c
+++ _printf.c
@@ -1,35 +1,34 @@
 #include "main.h"
 
 /**
- * _printf - Implementation of the inbuilt printf
- * @format: The format specifier
- *
- * Return: The formatted string
+ * _printf - implementation of the inbuilt printf
+ * @format: the format specifier
+ * Return: the formated string
  */
+
 int _printf(const char *format, ...)
 {
-    int printed = 0;
-    va_list args;
+	int printed = 0;
 
-    va_start(args, format);
+	va_list args;
 
-    while (*format != '\0')
-    {
-        if (*format == '%')
-        {
-            format++;
-            printed = selector(format, args, printed);
-            format++;
-        }
-        else
-        {
-            _putchar(*format);
-            printed++;
-            format++;
-        }
-    }
+	va_start(args, format);
 
-    va_end(args);
-    return (printed);
+	while (*format != '\0')
+	{
+		if (*format == '%')
+		{
+			format++;
+			printed = selector(format, args, printed);
+			format++;
+		}
+		else
+		{
+			_putchar(*format);
+			printed++;
+			format++;
+		}
+	}
+	va_end(args);
+	return (printed);
 }
-
